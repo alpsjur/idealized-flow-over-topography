58dfdf35ff1066fc56179f5f3f0038f0346523cb
alpsjur <alpsjur@runbox.com>
2023-09-27 17:36:08 +0200

    3d domain implemented, remains to set initial u

--- idealized_flow_over_topography/beta_plane_turbulence/2D_turbulence.jl
+++ idealized_flow_over_topography/beta_plane_turbulence/2D_turbulence.jl
@@ -1,8 +1,9 @@
 using Oceananigans
 using Statistics
 using CairoMakie
+using Printf
 
-β = 0.5
+β = 0
 
 grid = RectilinearGrid(size=(128,128), extent=(2π, 2π), topology=(Periodic, Bounded, Flat))
 
@@ -27,7 +28,14 @@ set!(model, u=uᵢ, v=vᵢ)
 
 simulation = Simulation(model, Δt=0.2, stop_time=600)
 
-progress(sim) = @info string("Iteration: ", iteration(sim), ", time: ", time(sim))
+# logging simulation progress
+start_time = time_ns()
+progress(sim) = @printf("i: % 6d, sim time: % 5.1f, wall time: % 15s, max |u|: % 5.3f\n",
+                        sim.model.clock.iteration,
+                        sim.model.clock.time,
+                        prettytime(1e-9 * (time_ns() - start_time)),
+                        maximum(abs, sim.model.velocities.u),
+                        )
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(100))
 
 u, v, w = model.velocities
@@ -40,30 +48,29 @@ U = Average(u, dims=1)
 PV = Average(ω, dims=1)
 
 filename = "2D_turbulence_β=$β"
+datapath = "idealized_flow_over_topography/beta_plane_turbulence/data/"
+animationpath = "idealized_flow_over_topography/beta_plane_turbulence/animations/"
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model, (; ω, s, u, v, U, PV),
+simulation.output_writers[:fields] = JLD2OutputWriter(model, (; ω, s, U, PV),
                                                       schedule = TimeInterval(0.3),
-                                                      filename = "data/"*filename*".jld2",
+                                                      filename = datapath*filename*".jld2",
                                                       overwrite_existing = true
                                                       )
 
 
 run!(simulation)
 
-ω_timeseries = FieldTimeSeries("data/"*filename*".jld2", "ω")
-s_timeseries = FieldTimeSeries("data/"*filename*".jld2", "s")
-u_timeseries = FieldTimeSeries("data/"*filename*".jld2", "u")
-v_timeseries = FieldTimeSeries("data/"*filename*".jld2", "v")
-U_timeseries = FieldTimeSeries("data/"*filename*".jld2", "U")
-PV_timeseries = FieldTimeSeries("data/"*filename*".jld2", "PV")
+ω_timeseries = FieldTimeSeries(datapath*filename*".jld2", "ω")
+s_timeseries = FieldTimeSeries(datapath*filename*".jld2", "s")
+U_timeseries = FieldTimeSeries(datapath*filename*".jld2", "U")
+PV_timeseries = FieldTimeSeries(datapath*filename*".jld2", "PV")
 
 
 times = ω_timeseries.times
 
 xω, yω, zω = nodes(ω_timeseries)
 xs, ys, zs = nodes(s_timeseries)
-xu, yu, zu = nodes(u_timeseries)
-xv, yv, zv = nodes(v_timeseries)
+xu, yu, zu = nodes(U_timeseries)
 
 
 set_theme!(Theme(fontsize=24))
@@ -83,15 +90,11 @@ ax_ω = Axis(fig[2,1]; title="Vorticity", axis_kwargs...)
 ax_s = Axis(fig[2,2]; title="Speed", axis_kwargs...)
 ax_PV = Axis(fig[3,1]; title="zonal mean ζ+βy", (xlabel="[m/s]", ylabel="y",limits=((-2,β*2π+2), (0,2π)))...)
 ax_U = Axis(fig[3,2]; title="zonal mean u", (xlabel="[1/s]", ylabel="y",limits=((-0.2,0.2), (0,2π)))...)
-#ax_u = Axis(fig[2,2]; title="zonal velocity", axis_kwargs...)
-#ax_v = Axis(fig[2,3]; title="meridional velocity", axis_kwargs...)
 
 n = Observable(1)
 
 ω = @lift interior(ω_timeseries[$n], :, :, 1)
 s = @lift interior(s_timeseries[$n], :, :, 1)
-u = @lift interior(u_timeseries[$n], :, :, 1)
-v = @lift interior(v_timeseries[$n], :, :, 1)
 U = @lift interior(U_timeseries[$n], 1, :, 1)
 PV = @lift interior(PV_timeseries[$n], 1, :, 1).+yω*β
 
@@ -101,9 +104,6 @@ heatmap!(ax_s, xs, ys, s; colormap=:speed, colorrange=(0,0.2))
 lines!(ax_U, U, yu)
 lines!(ax_PV, PV, yω)
 
-#heatmap!(ax_u, xu, yu, u; colormap=:balance, colorrange=(-0.2,0.2))
-#heatmap!(ax_v, xv, yv, v; colormap=:balance, colorrange=(-0.2,0.2))
-
 title = @lift "t="*string(round(times[$n],digits=2))
 Label(fig[1, 1:2], title, fontsize=24, tellwidth=false)
 
@@ -112,6 +112,6 @@ fig
 
 frames = 1:length(times)
 
-record(fig, "animations/"*filename*".mp4", frames, framerate=24) do i
+record(fig, animationpath*filename*".mp4", frames, framerate=24) do i
     n[] = i
 end
