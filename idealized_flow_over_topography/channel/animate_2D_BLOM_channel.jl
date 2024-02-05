39f588350f6187ed4476e34a7c11e8c0e3349d9e
alpsjur <alpsjur@runbox.com>
2024-02-05 18:30:05 +0100

    Initial script for 2D animation

--- idealized_flow_over_topography/channel/animate_2D_BLOM_channel.jl
+++ idealized_flow_over_topography/channel/animate_2D_BLOM_channel.jl
@@ -1,14 +1,13 @@
 using CairoMakie
 using Oceananigans
-using Oceananigans.Fields
-using Oceananigans.AbstractOperations: volume
+using Printf
 
 # based on https://clima.github.io/OceananigansDocumentation/stable/generated/horizontal_convection/
 
-saved_output_filename = "data/2D_BLOM_channel_test.jld2"
+saved_output_filename = "channel/data/2D_BLOM_channel_test.jld2"
 
 # Open the file with our data
-u_timeseries = FieldTimeSeries(saved_output_filename, "u")
+u_timeseries = FieldTimeSeries(saved_output_filename, "v")
 b_timeseries = FieldTimeSeries(saved_output_filename, "b")
 
 times = b_timeseries.times
@@ -24,8 +23,8 @@ n = Observable(1)
 
 title = @lift @sprintf("t=%1.2f", times[$n])
 
-uₙ = @lift interior(ζ_timeseries[$n], :, 1, :)
-bₙ = @lift interior(b_timeseries[$n], :, 1, :)
+uₙ = @lift interior(u_timeseries[$n], 1, :, :)
+bₙ = @lift interior(b_timeseries[$n], 1, :, :)
 
 ulim = 0.6
 blim = 0.6
@@ -45,12 +44,12 @@ ax_b = Axis(fig[3, 1];
 
 fig[1, :] = Label(fig, title, fontsize=24, tellwidth=false)
 
-hm_u = heatmap!(ax_u, xu, zu, uₙ;
+hm_u = heatmap!(ax_u, yu*1e-3, zu, uₙ;
                 colorrange = (-ulim, ulim),
                 colormap = :speed)
 Colorbar(fig[2, 2], hm_u)
 
-hm_b = heatmap!(ax_b, xc, zc, bₙ;
+hm_b = heatmap!(ax_b, yc*1e-3, zc, bₙ;
                 colorrange = (-blim, blim),
                 colormap = :thermal)
 Colorbar(fig[3, 2], hm_b)
@@ -58,7 +57,7 @@ Colorbar(fig[3, 2], hm_b)
 
 frames = 1:length(times)
 
-record(fig, "animations/2D_BLOM_channel.mp4", frames, framerate=8) do i
+record(fig, "channel/animations/2D_BLOM_channel.mp4", frames, framerate=8) do i
     msg = string("Plotting frame ", i, " of ", frames[end])
     print(msg * " \r")
     n[] = i
