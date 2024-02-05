d9143030e52c8c2d9ae7c2ad10faab4afc198716
alpsjur <alpsjur@runbox.com>
2024-02-05 18:29:49 +0100

    Separate diagnostics file

---
+++ idealized_flow_over_topography/channel/diagnostics_functions.jl
@@ -0,0 +1,26 @@
+
+"""
+This discussion should be useful too: https://github.com/CliMA/Oceananigans.jl/discussions/3363
+They're using the advective terms as an example, but basically it seems you can 
+define all the operations you need, like du/dt, (Av*u_z)_z, and pass them to your output writer.
+This way you should be able to save the momentum terms and close the budget exactly 
+at any output frequency you like. You might just have to define the time derivative separately. 
+That definition would have to match your simulation's time-stepping scheme, like what 
+they talk about regarding the advection scheme for the advective terms.
+
+a
+"""
+# time derivative of velocity 
+function ∂u∂t(model) 
+    # TODO fix first timestep
+    Gⁿ = model.timestepper.Gⁿ.u 
+    G⁻ = model.timestepper.G⁻.u 
+    χ  = model.timestepper.χ.u 
+
+    return (3/2 + χ) * Gⁿ - (1/2 + χ) * G⁻
+end
+
+#bottom friction force
+"""
+regne ut ved hjelp av likning for drag definert tidligere? Hvordan finne u ved bunn?
+"""
