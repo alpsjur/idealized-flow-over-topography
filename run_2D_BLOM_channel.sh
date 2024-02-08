7b73709d3057dc65c6fccfa275aa098128430d31
alpsjur <alpsjur@runbox.com>
2024-02-08 17:27:13 +0100

    one more line shift

--- run_2D_BLOM_channel.sh
+++ run_2D_BLOM_channel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice julia --project=. channel/2D_BLOM_channel.jl && echo -e "Skript ferdig! :) \nHilsen\n$(hostname)" | mail -s "2D_BLOM_channel.jl" alsjur@uio.no  || echo -e "Oi, her har det skjedd noe galt. Skript feila :( \nHilsen\n$(hostname)" | mail -s "2D_BLOM_channel.jl" alsjur@uio.no
+nice julia --project=. channel/2D_BLOM_channel.jl && echo -e "Skript ferdig! :) \n\nHilsen\n$(hostname)" | mail -s "2D_BLOM_channel.jl" alsjur@uio.no  || echo -e "Oi, her har det skjedd noe galt. Skript feila :( \n\nHilsen\n$(hostname)" | mail -s "2D_BLOM_channel.jl" alsjur@uio.no
