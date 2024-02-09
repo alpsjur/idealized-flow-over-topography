ffd020d4a77e566eac84c738f772d24caeecefbf
alpsjur <alpsjur@runbox.com>
2024-02-09 10:26:20 +0100

    removed typo/whitespaces

--- run_2D_BLOM_channel.sh
+++ run_2D_BLOM_channel.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-nice julia --project=. channel/2D_BLOM_channel.jl && echo -e "Skript ferdig! :) \n\nHilsen\n$(hostname)" | mail -s "2D_BLOM_channel.jl" alsjur@uio.no  || echo -e "Oi, her har det skjedd noe galt. Skript feila :( \n\nHilsen\n$(hostname)" | mail -s "2D_BLOM_channel.jl" alsjur@uio.no
+# Define the filename variable
+FILENAME="2D_BLOM_channel.jl"
+SUCSESS="Skript ferdig! :) \n\nHilsen\n$(hostname)"
+FAIL="Oi, her har det skjedd noe galt. Skript feila :( \nDet går bra, dette fikser du! \n\nHilsen\n$(hostname)"
+
+nice julia --project=. channel/$FILENAME && echo -e "$SUCSESS" | mail -s "$FILENAME" alsjur@uio.no  || echo -e "$FAIL" | mail -s "$FILENAME" alsjur@uio.no
