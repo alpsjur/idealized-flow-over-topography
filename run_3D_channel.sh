02a88923e4053e87518ffc7e5dc4f7665c396049
alpsjur <alpsjur@runbox.com>
2024-02-12 13:45:30 +0100

    script for running 3D

---
+++ run_3D_channel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Define the filename variable
+FILENAME="3D_channel.jl"
+SUCSESS="Skript ferdig! :) \n\nHilsen\n$(hostname)"
+FAIL="Oi, nå har det skjedd noe galt. Skript feila :( \nDet går bra, dette fikser du! \n\nHilsen\n$(hostname)"
+
+nice julia --project=. channel/$FILENAME && echo -e "$SUCSESS" | mail -s "$FILENAME" alsjur@uio.no alpsjur@runbox.com || echo -e "$FAIL" | mail -s "$FILENAME" alsjur@uio.no alpsjur@runbox.com
